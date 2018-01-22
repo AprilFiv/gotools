7f9908d0008f31330fb6fb01a90321642540e4da
af <lyx51b@126.com>
2018-01-22 16:22:47 +0800

    bugfix

--- aflog/aflog.go
+++ aflog/aflog.go
@@ -4,28 +4,43 @@ import (
 	"os"
 	"time"
 	"log"
+	"strings"
 )
 
 type Aflog struct {
 	appName string
+	logName string
 	logger  *log.Logger
+	fd *os.File
 }
-
+var currentLog Aflog
 func (log *Aflog) Info(content string) {
+	currentLog.checkTimeOut()
 	log.logger.SetPrefix("[Info] ")
 	log.logger.Println(content)
 }
 func (log *Aflog) Error(content string) {
+	currentLog.checkTimeOut()
 	log.logger.SetPrefix("[Error] ")
 	log.logger.Println(content)
 }
 func (log *Aflog) Debug(content string) {
+	currentLog.checkTimeOut()
 	log.logger.SetPrefix("[Debug] ")
 	log.logger.Println(content)
 }
 func (log *Aflog) Warn(content string) {
+	currentLog.checkTimeOut()
 	log.logger.SetPrefix("[Warn] ")
 	log.logger.Println(content)
+}
+func (log *Aflog) checkTimeOut(){
+	 var1 := strings.Split(log.logName,".")
+	 suffix :=var1[len(var1)]
+	 if suffix!=time.Now().Format("2006-01-02"){
+	 	log.fd.Close();
+	 	currentLog = GetLog(currentLog.appName)
+	 }
 }
 func pathExists(path string) (bool) {
 	_, err := os.Stat(path)
@@ -49,8 +64,11 @@ func GetLog(name string) Aflog {
 	logger = log.New(logFile, "[Info] ",log.LstdFlags)
 	aflog := Aflog{
 		appName: name,
+		logName: filePath,
 		logger:logger,
+		fd :logFile,
 	}
+	currentLog = aflog
 	return aflog
 }
 
