3ed495b3c7f4080bf337862f6712596b868f1439
af <lyx51b@126.com>
2018-01-25 16:32:11 +0800

    bug

--- aflog/aflog.go
+++ aflog/aflog.go
@@ -4,7 +4,6 @@ import (
 	"os"
 	"time"
 	"log"
-	"strings"
 )
 
 type Aflog struct {
@@ -13,35 +12,39 @@ type Aflog struct {
 	logger  *log.Logger
 	fd *os.File
 }
-var currentLog Aflog
-func (log *Aflog) Info(content string) {
-	currentLog.checkTimeOut()
-	log.logger.SetPrefix("[Info] ")
-	log.logger.Println(content)
+
+func (currentlog *Aflog) Info(content string) {
+	currentlog.flushLog()
+	currentlog.logger.SetPrefix("[Info] ")
+	currentlog.logger.Println(content)
+	currentlog.fd.Close()
 }
-func (log *Aflog) Error(content string) {
-	currentLog.checkTimeOut()
-	log.logger.SetPrefix("[Error] ")
-	log.logger.Println(content)
+func (currentlog *Aflog) Error(content string) {
+	currentlog.flushLog()
+	currentlog.logger.SetPrefix("[Error] ")
+	currentlog.logger.Println(content)
+	currentlog.fd.Close()
 }
-func (log *Aflog) Debug(content string) {
-	currentLog.checkTimeOut()
-	log.logger.SetPrefix("[Debug] ")
-	log.logger.Println(content)
+func (currentlog *Aflog) Debug(content string) {
+	currentlog.flushLog()
+	currentlog.logger.SetPrefix("[Debug] ")
+	currentlog.logger.Println(content)
+	currentlog.fd.Close()
 }
-func (log *Aflog) Warn(content string) {
-	currentLog.checkTimeOut()
-	log.logger.SetPrefix("[Warn] ")
-	log.logger.Println(content)
+func (currentlog *Aflog) Warn(content string) {
+	currentlog.flushLog()
+	currentlog.logger.SetPrefix("[Warn] ")
+	currentlog.logger.Println(content)
+	currentlog.fd.Close()
 }
-func (log *Aflog) checkTimeOut(){
-	 var1 := strings.Split(log.logName,".")
-	 suffix :=var1[len(var1)-1]
-	 if suffix!=time.Now().Format("2006-01-02"){
-	 	log.fd.Close();
-	 	currentLog = GetLog(currentLog.appName)
-	 }
-}
+//func (log *Aflog) checkTimeOut(){
+//	 var1 := strings.Split(log.logName,".")
+//	 suffix :=var1[len(var1)-1]
+//	 if suffix!=time.Now().Format("2006-01-02"){
+//	 	log.fd.Close();
+//	 	currentLog = GetLog(currentLog.appName)
+//	 }
+//}
 func pathExists(path string) (bool) {
 	_, err := os.Stat(path)
 	if err == nil {
@@ -68,7 +71,21 @@ func GetLog(name string) Aflog {
 		logger:logger,
 		fd :logFile,
 	}
-	currentLog = aflog
 	return aflog
+}
+func (currentlog *Aflog) flushLog(){
+	var logger *log.Logger
+	var logFile *os.File
+	filePath := currentlog.appName + ".log." + time.Now().Format("2006-01-02")
+	if !pathExists(filePath) {
+		logFile, _ = os.Create(filePath)
+		logger = log.New(logFile, "[Info] ",log.LstdFlags)
+	}
+	logFile,_ = os.OpenFile(filePath,os.O_APPEND,0)
+	logger = log.New(logFile, "[Info] ",log.LstdFlags)
+	currentlog.logger = logger
+	currentlog.fd = logFile
+	currentlog.logName = filePath
+
 }
 
